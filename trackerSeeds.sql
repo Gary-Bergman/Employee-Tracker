9d04758fafda821570d66ab62704ce8af386e687
Gary Bergman <garybergman00@gmail.com>
2020-07-08 17:02:27 -0400

    Created placeholder values for tables in Seeds file

---
+++ trackerSeeds.sql
@@ -0,0 +1,11 @@
+USE tracker_db;
+
+INSERT INTO employeeTbl (first_name, last_name, role_id, manager_id)
+VALUES("Test", "Tester", 5, 1);
+
+INSERT INTO roleTbl (title, salary, department_id)
+VALUES ("CEO", 100000, 1);
+
+INSERT INTO departmentTbl (name)
+VALUES ("Jane");
+
