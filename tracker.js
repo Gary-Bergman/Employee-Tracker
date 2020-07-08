adc91fa6357999fa56d608f8b2eaf7dffab54f16
Gary Bergman <garybergman00@gmail.com>
2020-07-08 16:48:05 -0400

    Created starter files and installed dependencies

---
+++ tracker.js
@@ -0,0 +1,22 @@
+// Required dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: process.env.MYSQL_PASS,
+    database: "tracker_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    // init();
+});
