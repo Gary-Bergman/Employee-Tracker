c725eceb5044af945b2c71b8df205bb029cb5d03
Gary Bergman <garybergman00@gmail.com>
2020-07-08 16:53:31 -0400

    Added tables in schema file

--- trackerSchema.sql
+++ trackerSchema.sql
@@ -3,3 +3,30 @@ DROP DATABASE IF EXISTS tracker_db;
 CREATE DATABASE tracker_db;
 
 use tracker_db;
+
+-- Table for employees
+CREATE TABLE employeeTbl (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+-- Table for roles
+CREATE TABLE roleTbl (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Table for departments
+CREATE TABLE departmentTbl (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
